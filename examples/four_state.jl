0ce29cc573cbb81f0949dc82821cc76670789450
sandreza <andrenogueirasouza@gmail.com>
2022-01-07 13:46:38 -0500

    all the estimates

--- examples/four_state.jl
+++ examples/four_state.jl
@@ -69,10 +69,10 @@ Dx = real.(ℱ⁻¹ * Diagonal(im .* k) * ℱ)
 x = reshape(x, (N, 1))
 z = reshape(z, (1, M + 1))
 
-ψ¹ = sin.(x) .* sin.(z)
-ψ² = cos.(x) .* sin.(z)
-ψ³ = -sin.(x) .* sin.(z)
-ψ⁴ = -cos.(x) .* sin.(z)
+ψ¹ = sin.(x) .* cos.(π / 2 * z)
+ψ² = cos.(x) .* cos.(π / 2 * z)
+ψ³ = -sin.(x) .* cos.(π / 2 * z)
+ψ⁴ = -cos.(x) .* cos.(π / 2 * z)
 
 ∂z = kron(Dz, I + zeros(N, N))
 ∂x = kron(I + zeros(M + 1, M + 1), Dx)
@@ -127,8 +127,9 @@ println(length(tmp.nzval))
 droprelativezeros!(tmp)
 println(length(tmp.nzval))
 
-γ = 1.0
-L = [(γ*I-Δ) -γ*I 0.5*A¹; γ*I (γ*I-Δ) -0.5*A²; A¹ -A² (2*γ*I-Δ)]
+γ = 10.0
+κ = 1e-2
+L = [(γ*I-κ*Δ) -γ*I 0.5*A¹; γ*I (γ*I-κ*Δ) -0.5*A²; A¹ -A² (2*γ*I-κ*Δ)]
 
 # Grab Boundary Indices
 zlifted = sparse(kron(Diagonal(z[:]), I + zeros(N, N)))
@@ -170,14 +171,16 @@ bools = real.(λE) .≤ eps(1e3 * maximum(abs.(λE)))
 sum(bools) == length(λE)
 
 maximum(abs.(EF²¹ + EF¹²))
-EF¹¹ *= -1.0 
+EF¹¹ *= -1.0
 EF¹² *= -1.0
-EF²¹ *= -1.0 
+EF²¹ *= -1.0
 EF²² *= -1.0
+
+makelocal(E) = Array(Diagonal(sum(E, dims = 2)[:]))
 ##
 
 using GLMakie
-fig = Figure(resolution = (1800, 1300))
+fig = Figure(resolution = (1800, 1300), title = "Nonlocal Operators")
 titlestring = "Kˣˣ"
 ax1 = Axis(fig[1, 1], title = titlestring, titlesize = 30)
 titlestring = "Kˣᶻ"
@@ -211,3 +214,100 @@ Colorbar(fig[2, 4], hm4, height = Relative(3 / 4), width = 25, ticklabelsize = 3
     labelsize = 30, ticksize = 25, tickalign = 1,)
 display(fig)
 ##
+# Local
+fig = Figure(resolution = (1800, 1300), title = "Local Operators")
+titlestring = "Kˣˣ"
+ax1 = Axis(fig[1, 1], title = titlestring, titlesize = 30)
+titlestring = "Kˣᶻ"
+ax2 = Axis(fig[2, 1], title = titlestring, titlesize = 30)
+titlestring = "Kᶻˣ"
+ax3 = Axis(fig[1, 3], title = titlestring, titlesize = 30)
+titlestring = "Kᶻᶻ"
+ax4 = Axis(fig[2, 3], title = titlestring, titlesize = 30)
+
+colormap = :thermal
+colormap2 = :balance
+hm1 = heatmap!(ax1, makelocal(EF¹¹), colormap = colormap)
+ax1.yreversed = true
+
+hm2 = heatmap!(ax2, makelocal(EF¹²), colormap = colormap2)
+ax2.yreversed = true
+
+hm3 = heatmap!(ax3, makelocal(EF²¹), colormap = colormap2)
+ax3.yreversed = true
+
+hm4 = heatmap!(ax4, makelocal(EF²²), colormap = colormap)
+ax4.yreversed = true
+
+Colorbar(fig[1, 2], hm1, height = Relative(3 / 4), width = 25, ticklabelsize = 30,
+    labelsize = 30, ticksize = 25, tickalign = 1,)
+Colorbar(fig[2, 2], hm2, height = Relative(3 / 4), width = 25, ticklabelsize = 30,
+    labelsize = 30, ticksize = 25, tickalign = 1,)
+Colorbar(fig[1, 4], hm3, height = Relative(3 / 4), width = 25, ticklabelsize = 30,
+    labelsize = 30, ticksize = 25, tickalign = 1,)
+Colorbar(fig[2, 4], hm4, height = Relative(3 / 4), width = 25, ticklabelsize = 30,
+    labelsize = 30, ticksize = 25, tickalign = 1,)
+display(fig)
+
+##
+
+function avglocaldiffusivity(E, N, M)
+    local localdiff = makelocal(E)
+    localdiff = [localdiff[i, i] for i = 1:size(E)[1]]
+    local localdiff = reshape(localdiff, (N, M + 1))
+    local localdiff = sum(localdiff, dims = 1)[:]
+    return localdiff
+end
+
+localdiff = avglocaldiffusivity(EF¹¹, N, M)
+lines(localdiff, z[:])
+##
+fig = Figure(resolution = (1800, 1300), title = "Local Operators")
+titlestring = "Kˣˣ"
+ax1 = Axis(fig[1, 1], title = titlestring, titlesize = 30)
+titlestring = "Kˣᶻ"
+ax2 = Axis(fig[2, 1], title = titlestring, titlesize = 30)
+titlestring = "Kᶻˣ"
+ax3 = Axis(fig[1, 2], title = titlestring, titlesize = 30)
+titlestring = "Kᶻᶻ"
+ax4 = Axis(fig[2, 2], title = titlestring, titlesize = 30)
+
+colormap = :thermal
+colormap2 = :balance
+hm1 = lines!(ax1, avglocaldiffusivity(EF¹¹, N, M), z[:])
+hm2 = lines!(ax2, avglocaldiffusivity(EF¹², N, M), z[:])
+hm3 = lines!(ax3, avglocaldiffusivity(EF²¹, N, M), z[:])
+hm4 = lines!(ax4, avglocaldiffusivity(EF²², N, M), z[:])
+
+display(fig)
+
+##
+# Stream Functions 
+fig = Figure(resolution = (1800, 1300), title = "Local Operators")
+titlestring = "ψ¹"
+ax1 = Axis(fig[1, 1], title = titlestring, titlesize = 30)
+titlestring = "ψ²"
+ax2 = Axis(fig[1, 3], title = titlestring, titlesize = 30)
+titlestring = "ψ³"
+ax3 = Axis(fig[2, 1], title = titlestring, titlesize = 30)
+titlestring = "ψ⁴"
+ax4 = Axis(fig[2, 3], title = titlestring, titlesize = 30)
+
+colormap = :balance
+hm1 = heatmap!(ax1, ψ¹, colormap = colormap)
+
+hm2 = heatmap!(ax2, ψ², colormap = colormap)
+
+hm3 = heatmap!(ax3, ψ³, colormap = colormap)
+
+hm4 = heatmap!(ax4, ψ⁴, colormap = colormap)
+
+Colorbar(fig[1, 2], hm1, height = Relative(3 / 4), width = 25, ticklabelsize = 30,
+    labelsize = 30, ticksize = 25, tickalign = 1,)
+Colorbar(fig[2, 2], hm2, height = Relative(3 / 4), width = 25, ticklabelsize = 30,
+    labelsize = 30, ticksize = 25, tickalign = 1,)
+Colorbar(fig[1, 4], hm3, height = Relative(3 / 4), width = 25, ticklabelsize = 30,
+    labelsize = 30, ticksize = 25, tickalign = 1,)
+Colorbar(fig[2, 4], hm4, height = Relative(3 / 4), width = 25, ticklabelsize = 30,
+    labelsize = 30, ticksize = 25, tickalign = 1,)
+display(fig)
