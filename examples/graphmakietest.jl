5d7d9ca25d1eb2b5d6f4eb5ad42841fe0aa1b3db
Andre Souza <andrenogueirasouza@gmail.com>
2022-07-05 09:49:14 -0400

    add correct imports

--- examples/graphmakietest.jl
+++ examples/graphmakietest.jl
@@ -1,4 +1,8 @@
-using StatisticalNonlocality, Distributions, Random, LinearAlgebra
+using StatisticalNonlocality
+using Distributions, Random, LinearAlgebra
+using Graphs
+using GLMakie, GraphMakie
+
 import StatisticalNonlocality: ou_transition_matrix
 import StatisticalNonlocality: uniform_phase
 
@@ -6,20 +10,18 @@ cmap = :balance # :Blues_9
 cmapa = RGBAf.(to_colormap(cmap), 1);
 cmap = vcat(cmapa[1:15], fill(RGBAf(0, 0, 0, 0), 10), cmapa[25:end])
 
-
 fig = Figure()
 ax = Axis(fig[1, 1]; title="Transition Probability", titlesize=30)
 ax_Q = Axis(fig[1, 2]; title="Transition Rate", titlesize=30)
 
 dt_slider = Slider(fig[2, 1:2], range=0:0.01:2, startvalue=0)
 dt = dt_slider.value
-Q = ou_transition_matrix(4)
-Q = uniform_phase(4)
+# Q = ou_transition_matrix(4)
+Q = uniform_phase(8)
 T = @lift exp(Q * $dt)
 
 g = DiGraph(exp(Q))
 g_Q = DiGraph(Q)
-
 
 edge_color = @lift [RGBAf(cmapa[4].r, cmapa[4].g, cmap[4].b, $T[i]) for i in 1:ne(g)]
 edge_width = [4.0 for i in 1:ne(g)]
@@ -28,10 +30,11 @@ arrow_size = [30.0 for i in 1:ne(g)]
 edge_color_Q = [RGBAf(cmapa[4].r, cmapa[4].g, cmap[4].b, 1.0) for i in 1:ne(g_Q)]
 edge_width_Q = [4.0 for i in 1:ne(g_Q)]
 arrow_size_Q = [30.0 for i in 1:ne(g_Q)]
+node_size = 3.0
 
-obs_string = @lift("Transition Probability at time t = " * string($dt) )
-graphplot!(ax, g, edge_color=edge_color, edge_width=edge_width, arrow_size=arrow_size)
+# obs_string = @lift("Transition Probability at time t = " * string($dt) )
+graphplot!(ax, g, edge_color=edge_color, edge_width=edge_width, arrow_size=arrow_size, node_size = node_size)
 
-graphplot!(ax_Q, g_Q, edge_color=edge_color_Q, edge_width=edge_width_Q, arrow_size=arrow_size_Q)
+graphplot!(ax_Q, g_Q, edge_color=edge_color_Q, edge_width=edge_width_Q, arrow_size=arrow_size_Q, node_size = node_size)
 display(fig)
 
