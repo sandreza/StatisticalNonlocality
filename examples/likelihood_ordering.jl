355ef2ebcac7d2cda0b2dd14e2cdf719e4be7afd
Andre Souza <andrenogueirasouza@gmail.com>
2022-08-13 16:04:48 -0400

    cyclic reduction

--- examples/likelihood_ordering.jl
+++ examples/likelihood_ordering.jl
@@ -14,4 +14,22 @@ for i in ProgressBar(1:length(snapshots))
     push!(mpp_dynamics_end, union(mpp_dynamics[end-length(snapshots):end]))
 end
 cyclic_groups = union(mpp_dynamics_end)
+cyclic_groups[5]
+cyclic_groups[6]
+generators = [sort(generator)[1] for generator in cyclic_groups]
+generators = union(generators)
+
+mpp_dynamics_end = []
+for i in ProgressBar(generators)
+    initial_i = i
+    mpp_dynamics = Int64[]
+    for i in 1:100*length(snapshots)
+        push!(mpp_dynamics, mpp(initial_i))
+        initial_i = mpp(initial_i)
+    end
+    push!(mpp_dynamics_end, union(mpp_dynamics[end-length(snapshots):end]))
+end
+
+## Reduced Dynamics: 
+# sum(length.(cyclic_groups))
 
