87c122806aa1b22fc9a3cd9d8372a999671061d4
Andre Souza <andrenogueirasouza@gmail.com>
2022-10-11 19:45:16 -0400

    autocorrelation plot

--- examples/lorenz_coarse.jl
+++ examples/lorenz_coarse.jl
@@ -138,7 +138,7 @@ tuple_state = Tuple.(state)
 fig = Figure(resolution = (2000, 1500)) 
 ax = LScene(fig[1:2,1:2]) # title = "Phase Space and Partitions", titlesize = 40)
 ax2 = Axis(fig[1,3]; title = "Markov Embedding ", titlesize = 30)
-ax3 = Axis(fig[2,3]; title = "Transition Rate Matrix", titlesize = 30)
+ax3 = Axis(fig[2,3]; title = "Transition Rate Matrix", titlesize = 30) # perhaps call generator? 
 lorenz_slider = Slider(fig[3, 1:2], range=3:1:1000000, startvalue=1000)
 observable_index = lorenz_slider.value
 plot_state = @lift(tuple_state[2:$observable_index+1])
@@ -198,7 +198,7 @@ function change_function(time_index)
     end
 end
 
-record(change_function, fig, "lorenz_animation_3.mp4", time_indices; framerate = framerate)
+# record(change_function, fig, "lorenz_animation_3.mp4", time_indices; framerate = framerate)
 
 ##
 scatter(current_state[end-10000:end])
@@ -228,6 +228,7 @@ r_perron_frobenius = count_matrix' ./ sum(count_matrix', dims =1)
 Q = transition_rate_matrix(current_state, length(markov_states); γ=dt);
 rQ = transition_rate_matrix(reverse(current_state), length(markov_states); γ=dt)
 Λ, V =  eigen(Q)
+iV = inv(V)
 p = real.(V[:,end] ./ sum(V[:,end]))
 entropy = sum(-p .* log.(p) / log(length(markov_states)))
 println("The entropy is ", entropy) # uniform distribution for a given N is always assigned to be one
@@ -247,69 +248,90 @@ mean(pys)
 mean(pzs)
 
 ##
-snapshots = markov_states
-state_timeseries = state
-reaction_coordinate(u) = u[3]# u[1] / (0.1 + u[3]) * 3/8# norm(u) # - mean(u[4][1+shiftx:15+shiftx, 1+shifty:15+shifty, end-2], dims =(1,2))[1] .* mean(u[2][1+shiftx:15+shiftx, 1+shifty:15+shifty, end-2], dims=(1, 2))[1]  # u[2][120, 30, end-0] * u[4][120, 30, end-0] # mean(u[1][:, 30, end], dims=1)[1] # * u[4][140, 60, end-2] # * u[1][130, 60, end-1] # * u[4][140+0, 60+0, end]  # distance(u, snapshots[4])# real(iV[1, argmin([distance(u, s) for s in snapshots])])
-markov = [reaction_coordinate(snapshot) for snapshot in snapshots]
-rtimeseries = [reaction_coordinate(state) for state in state_timeseries]
-xs_m, ys_m = histogram(markov, normalization=p, bins=20, custom_range=extrema(rtimeseries))
-xs_t, ys_t = histogram(rtimeseries, bins=20, custom_range=extrema(rtimeseries))
-fig = Figure()
-kwargs = (; ylabel="probability", titlesize=30, ylabelsize=30)
-ax1 = Axis(fig[1, 1]; title="Ensemble Statistics", kwargs...)
-ax2 = Axis(fig[1, 2]; title="Temporal Statistics", kwargs...)
-barplot!(ax1, xs_m, ys_m, color=:red)
-barplot!(ax2, xs_t, ys_t, color=:blue)
+##
+Q = transition_rate_matrix(current_state, length(markov_states); γ=dt);
+Λ, V = eigen(Q)
+iV = inv(V)
+p = real.(V[:, end] ./ sum(V[:, end]))
+perron_frobenius = exp(Q * dt)
 
-for ax in [ax1, ax2]
-    x_min = minimum([minimum(xs_m), minimum(xs_t)])
-    x_max = maximum([maximum(xs_m), maximum(xs_t)])
-    y_min = minimum([minimum(ys_m), minimum(ys_t)])
-    y_max = maximum([maximum(ys_m), maximum(ys_t)])
-    xlims!(ax, (x_min, x_max))
-    ylims!(ax, (y_min, y_max))
+hfig = Figure(resolution = (1800, 1500))
+xfig = hfig[1, 1] = GridLayout()
+yfig = hfig[2, 1] = GridLayout()
+zfig = hfig[3, 1] = GridLayout()
+subfigs = [xfig, yfig, zfig]
+colors = [:red, :blue, :orange]
+labels = ["x", "y", "z"]
+
+# reaction_coordinate(u) = real(iV[1, argmin([norm(u - s) for s in markov_states])]) # u[3] # 
+reaction_coordinates =  [u -> u[i] for i in 1:3] # define anonymous functions for reaction coordinates
+kwargs = (; ylabel="probability", titlesize=30, ylabelsize=40, xgridstyle=:dash, ygridstyle=:dash, xtickalign=1,
+    xticksize=20, ytickalign=1, yticksize=20,
+    xticklabelsize=40, yticklabelsize=40)
+bins1 = 30
+bins2 = 20
+
+for i in 1:3
+    current_reaction_coordinate = reaction_coordinates[i]
+    subfig = subfigs[i]
+
+    markov = [current_reaction_coordinate(snapshot) for snapshot in snapshots]
+    rtimeseries = [current_reaction_coordinate(state) for state in state_timeseries]
+    xs_m, ys_m = histogram(markov, normalization=p, bins=bins1, custom_range=extrema(rtimeseries))
+    xs_t, ys_t = histogram(rtimeseries, bins=bins2, custom_range=extrema(rtimeseries))
+
+
+    ax1 = Axis(subfig[1, 1]; title="Markov Chain Histogram, " * labels[i], kwargs...)
+    ax2 = Axis(subfig[1, 2]; title="Timeseries Histogram, " * labels[i], kwargs...)
+
+    for ax in [ax1, ax2]
+        x_min = minimum([minimum(xs_m), minimum(xs_t)])
+        x_max = maximum([maximum(xs_m), maximum(xs_t)])
+        y_min = minimum([minimum(ys_m), minimum(ys_t)])
+        y_max = maximum([maximum(ys_m), maximum(ys_t)])
+        xlims!(ax, (x_min, x_max))
+        ylims!(ax, (y_min, y_max))
+    end
+
+    barplot!(ax1, xs_m, ys_m, color= :purple)
+    barplot!(ax2, xs_t, ys_t, color=:black)
+    hideydecorations!(ax2, grid=false)
+
+
 end
-
-display(fig)
-ensemble_mean = sum(p .* markov)
-temporal_mean = mean(rtimeseries)
-ensemble_variance = sum(p .* (markov .^ 2)) - sum(p .* markov)^2
-temporal_variance = mean(rtimeseries .^ 2) - mean(rtimeseries)^2
-println("The ensemble mean is ", ensemble_mean)
-println("The temporal mean is ", temporal_mean)
-println("The null hypothesis is ", mean(markov))
-println("The ensemble variance is ", ensemble_variance)
-println("The temporal variance is ", temporal_variance)
-println("The null hypothesis is ", var(markov))
-println("The absolute error between the ensemble and temporal means is ", abs(ensemble_mean - temporal_mean))
-println("The relative error between the ensemble and temporal variances are ", 100 * abs(ensemble_variance - temporal_variance) / temporal_variance, " percent")
+display(hfig)
 
 ##
-total = 800# *3*3
-auto_correlation_timeseries = zeros(total)
-for s in ProgressBar(0:total-1)
-    auto_correlation_timeseries[s+1] = mean(rtimeseries[s+1:end] .* rtimeseries[1:end-s])
+tmp = []
+tmp_labels = []
+for i in 1:3
+    push!(tmp, u -> u[i] )
+    push!(tmp_labels, labels[i])
 end
-auto_correlation_timeseries .-= mean(rtimeseries)^2
-auto_correlation_timeseries .*= 1 / auto_correlation_timeseries[1]
+for i in 1:3 
+    for j in i:3
+        push!(tmp, u -> u[i]*u[j])
+        push!(tmp_labels, labels[i] * labels[j])
+    end
+end
 
-auto_correlation_snapshots = zeros(total)
-val = [reaction_coordinate(snapshot) for snapshot in snapshots]
-Pτ = perron_frobenius * 0 + I
-for i in ProgressBar(0:total-1)
-    auto_correlation_snapshots[i+1] = sum(val' * Pτ * (p .* val))
-    Pτ *= perron_frobenius
+for i in eachindex(tmp_labels)
+    tmpf = tmp[i]
+    # println(" ensemble average of $(tmp_labels[i]) = $(sum(tmpf.(markov_states) .* p))")
+    # println(" temporal average of $(tmp_labels[i]) = $(mean(tmpf.(state_timeseries)))")
+    println(" ensemble: ⟨$(tmp_labels[i])⟩ = $(sum(tmpf.(markov_states) .* p))")
+    println(" temporal: ⟨$(tmp_labels[i])⟩ = $(mean(tmpf.(state_timeseries)))")
+    println("--------------------------------------------")
 end
-auto_correlation_snapshots .= auto_correlation_snapshots .- sum(val .* p)^2;
-auto_correlation_snapshots .*= 1.0 / auto_correlation_snapshots[1];
+
 ##
 # check the holding times 
 ht = construct_holding_times(current_state, 3; γ = dt)
 bins = [5, 20, 100]
 color_choices = [:red, :blue, :orange] # same convention as before
 index_names = ["Negative Lobe", "Origin", "Positive Lobe"]
-hi = 1
-bin_index = 1
+hi = 1 #holding index
+bin_index = 1 # bin index
 labelsize = 40
 options = (; xlabel="Time", ylabel="Probability", titlesize=labelsize, ylabelsize=labelsize, xlabelsize=labelsize, xticklabelsize = labelsize, yticklabelsize = labelsize)
 fig = Figure(resolution = (2800, 1800)) 
@@ -322,21 +344,72 @@ for hi in 1:3, bin_index in 1:3
 
     barplot!(ax, holding_time, holding_time_probability, color=color_choices[hi], label = "Data")
     λ = 1/mean(ht[holding_time_index])
-    # holding_time_fine = range(0, 30 / λ, length=100000)
+
     Δholding_time = holding_time[2] - holding_time[1]
     exponential_distribution = @. (exp( - λ  * (holding_time - 0.5 *  Δholding_time)) - exp( - λ  * (holding_time + 0.5 *  Δholding_time)) ) 
     lines!(ax, holding_time, exponential_distribution, color = :black, linewidth = 3)
-    scatter!(ax, holding_time, exponential_distribution, color = :black, markersize = 10, label = "Analytic")
+    scatter!(ax, holding_time, exponential_distribution, color = :black, markersize = 10, label = "Exponential")
     axislegend(ax, position = :rt, framecolor = (:grey, 0.5), patchsize = (50, 50), markersize = 100, labelsize = 40)
 end
 display(fig)
+
 ##
-auto_fig = Figure()
-ax1 = Axis(auto_fig[1, 1]; title="Ensemble Statistics")
-l1 = lines!(ax1, auto_correlation_snapshots[:], color=:red)
-l2 = lines!(ax1, auto_correlation_timeseries[:], color=:blue)
-Legend(auto_fig[1, 2], [l1, l2], ["Markov", "Timeseries"])
+auto_fig = Figure(resolution=(2700, 1800))
+xfig = auto_fig[1, 1] = GridLayout()
+yfig = auto_fig[2, 1] = GridLayout()
+zfig = auto_fig[3, 1] = GridLayout()
+koopman1_fig = auto_fig[1, 2] = GridLayout()
+koopman2_fig = auto_fig[2, 2] = GridLayout()
+z10_fig = auto_fig[3, 2] = GridLayout()
+subfigs = [xfig, yfig, zfig, koopman1_fig, koopman2_fig, z10_fig]
+colors = [:red, :blue, :orange]
+labels = ["x", "y", "z", "koopman1", "koopman2", "1_{x <= 0}"]
+
+reaction_coordinates = [u -> u[i] for i in 1:3] # define anonymous functions for reaction coordinates
+reaction_coordinates = [reaction_coordinates..., [u -> real(iV[i, argmin([norm(u - s) for s in markov_states])]) for i in [2, 1]]..., u -> (u[1] <= 0)]
+# reaction_coordinates = [u -> u[1], u -> u[2], u -> u[3], u -> real(iV[2, argmin([norm(u - s) for s in markov_states])]), u -> real(iV[1, argmin([norm(u - s) for s in markov_states])]), u -> (u[3] < 10)]
+
+kwargs = (; ylabel="Autocorrelation", titlesize=30, ylabelsize=40, 
+    xgridstyle=:dash, ygridstyle=:dash, ygridwidth=5, xgridwidth = 5,  xtickalign=1,
+    xticksize=20, ytickalign=1, yticksize=20, xlabel = "Time", 
+    xticklabelsize=40, yticklabelsize=40, xlabelsize = 40)
+
+
+for i in ProgressBar(1:6)
+    current_reaction_coordinate = reaction_coordinates[i]
+    subfig = subfigs[i]
+
+    markov = [current_reaction_coordinate(snapshot) for snapshot in snapshots]
+    rtimeseries = [current_reaction_coordinate(state) for state in state_timeseries]
+
+    total = 800
+    auto_correlation_timeseries = zeros(total)
+    for s in ProgressBar(0:total-1)
+        auto_correlation_timeseries[s+1] = mean(rtimeseries[s+1:end] .* rtimeseries[1:end-s])
+    end
+    auto_correlation_timeseries .-= mean(rtimeseries)^2
+    auto_correlation_timeseries .*= 1 / auto_correlation_timeseries[1]
+
+    auto_correlation_snapshots = zeros(total)
+    val = [current_reaction_coordinate(snapshot) for snapshot in snapshots]
+
+    Pτ = perron_frobenius * 0 + I
+    for i in 0:total-1
+        auto_correlation_snapshots[i+1] = sum(val' * Pτ * (p .* val))
+        Pτ *= perron_frobenius
+    end
+    auto_correlation_snapshots .= auto_correlation_snapshots .- sum(val .* p)^2
+    auto_correlation_snapshots .*= 1.0 / auto_correlation_snapshots[1]
+
+
+    ax1 = Axis(subfig[1, 1]; title= "Variable =  " * labels[i], kwargs...)
+    l1 = lines!(ax1, dt .* collect(0:total-1), auto_correlation_snapshots[:], color=:purple, label="Markov", linewidth = 5)
+    l2 = lines!(ax1, dt .* collect(0:total-1), auto_correlation_timeseries[:], color=:black, label="Timeseries", linewidth = 5)
+    axislegend(ax1, position=:rt, framecolor=(:grey, 0.5), patchsize=(50, 50), markersize=100, labelsize=40)
+    display(auto_fig)
+end
 display(auto_fig)
+
 
 ##
 function coarse_grain_operator(identified_states)
