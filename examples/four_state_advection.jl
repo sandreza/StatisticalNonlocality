d364293854f9963a4714082b70b8fceb85008145
sandreza <andrenogueirasouza@gmail.com>
2023-03-19 10:58:00 -0400

    four state advection update

--- examples/four_state_advection.jl
+++ examples/four_state_advection.jl
@@ -1,10 +1,12 @@
 using StatisticalNonlocality, LinearAlgebra, FFTW, SparseArrays, JLD2
 import StatisticalNonlocality: chebyshev, fourier_nodes, fourier_wavenumbers
 import StatisticalNonlocality: droprelativezeros!
+using CUDA
+array_type = CuArray
 
 # minimal configuration is M = 64, N = 8
 N = 8 * 4
-M = 8 * 4
+M = 8 * 4 
 
 # The first step is to build the block operators
 a, b = 0, 4π
@@ -12,25 +14,25 @@ k = fourier_wavenumbers(N, L=b - a)
 x = fourier_nodes(N, a=a, b=b)
 Dz, z = chebyshev(M)
 
-function build_operator_dirichlet(Dz, k)
+function build_operator_dirichlet(Dz, k; array_type = Array)
     A = Dz * Dz .- k^2
     A[1, :] .= 0.0
     A[1, 1] = 1.0
     A[end, :] .= 0
     A[end, end] = 1.0
-    return lu(A)
+    return lu(array_type(A))
 end
-function build_operator_neuman(Dz, k)
+function build_operator_neuman(Dz, k; array_type = Array)
     A = Dz * Dz .- k^2
     A[1, :] .= Dz[1,:]
     A[end, :] = Dz[end, :]
-    return lu(A)
+    return lu(array_type(A))
 end
 
+lu_fact = [build_operator_neuman(Dz, k[i]; array_type = array_type) for i in 1:round(Int,N/2)]
 
-lu_fact = [build_operator_neumann(Dz, k[i]) for i in 1:round(Int,N/2)]
-
-rhs = ones(33, 1000)
+rhs = ones(size(Dz)[1], 1000)
 rhs[1, :] .= 0.0
 rhs[end, :] .= 0.0
+rhs = array_type(rhs)
 sol = lu_fact[2] \ rhs
